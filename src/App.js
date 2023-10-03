8336f8f870a2b888a29dafed5a5f5cd6aa1d18ab
lalitjakhar <lalitkumarjakhar22216@gmail.com>
2023-10-03 18:55:05 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Home from './Components/Home/Home';
+import About from './Components/About/About';
 
 const App = () => {
   return (
     <>
       <Home />
+      <About />
     </>
   );
 }
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -4,28 +4,29 @@ import { Delete } from '@mui/icons-material';
 import Button from '@mui/material/Button';
 
 function Home() {
-  const [color,setColor]=useState("secondary")
-  const[disableBtn ,setdisableBtn]=useState(false)
+    const [color, setColor] = useState("secondary")
+    const [disableBtn, setdisableBtn] = useState(false)
 
-  function customMe()
-  {
-    // setColor("secondary")
-    setdisableBtn("true")
-    alert("function called")
-  }
-  return (
-    <div className="Home">
-      <h1>React Material UI</h1>
-      <Button
-      color={color}
-      disabled={disableBtn}
-      variant="contained"
-      onclick={()=>{customElements()}}
-      startIcon={<Delete />}
-      >Click Me
-      </Button>
-    </div>
-  );
+    function customMe() {
+        // setColor("secondary")
+        setdisableBtn("true")
+        alert("function called")
+    }
+    return (
+        <>
+            <div className="Home">
+                <h1>React Material UI</h1>
+                <Button
+                    color={color}
+                    disabled={disableBtn}
+                    variant="contained"
+                    onclick={() => { customElements() }}
+                    endIcon={<Delete />}
+                >Click Me Click Me
+                </Button>
+            </div>
+        </>
+    );
 }
 
 export default Home;
